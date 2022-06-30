9a450972bb6054dc396bd8fae2ed9bf0bd529dbb
Ebuka0101 <55116310+Ebuka0101@users.noreply.github.com>
2022-06-30 09:30:00 -0400

    Updates

---
+++ precache-manifest.df67686463ce971d54baba18beefe23a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f75e20ed0b2085f16d70",
+    "url": "/motive-react/static/css/main.87b5ec0c.chunk.css"
+  },
+  {
+    "revision": "f75e20ed0b2085f16d70",
+    "url": "/motive-react/static/js/main.f75e20ed.chunk.js"
+  },
+  {
+    "revision": "5ee476ae1244b517c019",
+    "url": "/motive-react/static/js/1.5ee476ae.chunk.js"
+  },
+  {
+    "revision": "a32495fe3634d6be5643",
+    "url": "/motive-react/static/css/2.e0ab3ecf.chunk.css"
+  },
+  {
+    "revision": "a32495fe3634d6be5643",
+    "url": "/motive-react/static/js/2.a32495fe.chunk.js"
+  },
+  {
+    "revision": "7d2a976353b6d0059e63",
+    "url": "/motive-react/static/js/runtime~main.7d2a9763.js"
+  },
+  {
+    "revision": "9f0fb83fa06141932300cdeac846567e",
+    "url": "/motive-react/static/media/5f89f58d2dfac88175937119cecca63e.9f0fb83f.png"
+  },
+  {
+    "revision": "4d86731e577a28d0170cc0b9fa8da32e",
+    "url": "/motive-react/static/media/d9aeb53d702512f3574e5f4d25f13a39.4d86731e.png"
+  },
+  {
+    "revision": "3fd973901700f6044bbd5dfbd473c5ce",
+    "url": "/motive-react/static/media/6e7e7722f9f8dc8ca912680948193ddc.3fd97390.png"
+  },
+  {
+    "revision": "47bb99dff8a85e2a3718bfdf172ce11a",
+    "url": "/motive-react/index.html"
+  }
+];
