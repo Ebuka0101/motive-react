7a80599a7cd0a272bd81941ee45572e1d77641d6
Ebuka0101 <55116310+Ebuka0101@users.noreply.github.com>
2022-06-30 10:32:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,17 +17,18 @@ function App() {
         <nav>
           <h1 style={{fontFamily: "Special Elite"}}>Motivv</h1>
           {/* <Link to='/landing'>Landing </Link> */}
-          <Link to='/'>Home </Link>
-          <Link to='/plan'>Plan </Link>
-          <Link to='/help'>Help </Link>
-          <Link to="/eventType">Event Type</Link>
+          <Link to='/motive-react/'>Home </Link>
+          <Link to='/motive-react/plan'>Plan </Link>
+          <Link to='/motive-react/help'>Help </Link>
+          <Link to="/motive-react/eventType">Event Type</Link>
         </nav>
+        <p>Rounter is buggy at the moment</p>
         <Routes>
           {/* <Route path="/landing" element={<LandingPage/>} /> */}
-          <Route exact path='/' element={<Home />} />   
-          <Route path='/plan' element={ <Plan />} /> 
-          <Route path='/help' element={<ContactUs />} />
-          <Route path='/eventType' element={<EventType />} />
+          <Route exact path='/motive-react/' element={<Home />} />   
+          <Route path='/motive-react/plan' element={ <Plan />} /> 
+          <Route path='/motive-react/help' element={<ContactUs />} />
+          <Route path='/motive-react/eventType' element={<EventType />} />
         </Routes>
       </BrowserRouter>
       {/* <EventType /> */}
